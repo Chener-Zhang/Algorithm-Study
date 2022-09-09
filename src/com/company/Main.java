1c542d87fd9a120331259188f2e6b2ca3ea85c97
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-09-09 13:38:38 -0700

    create the heapsort needs to debug

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,16 +10,12 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        //Defind Sort method
-        QuickSort quickSort = new QuickSort();
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        int[] arr = new int[]{5, 8, 3, 9, 4, 1, 7};
+        RandomNumberGenerator(15);
+        int[] arr = new int[]{4, 13, 13, 6, 4, 0, 5, 8, 7, 3};
+        ArrayList<Integer> list = new ArrayList<>();
         for (int i : arr) {
             list.add(i);
         }
-        quickSort.sort(list, 0, list.size() - 1);
-        System.out.println(list.toString());
-//        RandomNumberGenerator(20);
     }
 
     public static ArrayList<Integer> RandomNumberGenerator(int upperbound) {
--- src/com/company/SortingAlgorithem/HeapSort.java
+++ src/com/company/SortingAlgorithem/HeapSort.java
@@ -1,7 +1,50 @@
 package com.company.SortingAlgorithem;
 
+import java.util.ArrayList;
+
 public class HeapSort {
-    void heaplify() {
+    public HeapSort() {
 
     }
+
+    public ArrayList<Integer> heap_sort(ArrayList<Integer> arr) {
+        //start with the last has child node
+        int start = (arr.size() - 1) / 2;
+        int len = arr.size();
+        for (int i = start; i >= 0; i--) {
+            heaplify(arr, len - 1, i);
+        }
+
+        System.out.println(arr.toString());
+        for (int i = len - 1; i > 0; i--) {
+            swap(arr, i, 0);
+            heaplify(arr, len, 0);
+        }
+        System.out.println(arr.toString());
+        return null;
+    }
+
+    public void heaplify(ArrayList<Integer> arr, int size, int i) {
+        int max = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < size && arr.get(left) > arr.get(max)) {
+            max = left;
+        }
+        if (right < size && arr.get(right) > arr.get(max)) {
+            max = right;
+        }
+        if (i != max) {
+            swap(arr, i, max);
+            heaplify(arr, size, max);
+        }
+    }
+
+    public void swap(ArrayList<Integer> arr, int a, int b) {
+        int temp = arr.get(a);
+        arr.set(a, arr.get(b));
+        arr.set(b, temp);
+    }
+
 }
