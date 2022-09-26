707bd9fe568ed3637a8b372d185272eb3fd3b034
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-09-26 11:49:15 -0700

    + iterativeSolutionLevelOrder with iterative solution of queue

--- src/com/company/TreeAlgorithm/levelOrderTraversal.java
+++ src/com/company/TreeAlgorithm/levelOrderTraversal.java
@@ -1,12 +1,33 @@
 package com.company.TreeAlgorithm;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class levelOrderTraversal {
     public List<List<Integer>> res = new ArrayList<>();
 
-    public List<List<Integer>> levelOrder(TreeNode root) {
+    public List<List<Integer>> iterativeSolutionLevelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> temp = new ArrayList<>();
+
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.remove();
+                temp.add(current.val);
+                if (current.left != null) queue.add(current.left);
+                if (current.right != null) queue.add(current.right);
+            }
+            res.add(temp);
+        }
+        return res;
+    }
+
+    public List<List<Integer>> levelOrderRecursion(TreeNode root) {
         helper(root, 0);
         return res;
     }
