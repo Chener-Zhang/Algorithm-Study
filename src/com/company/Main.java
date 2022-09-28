8be6869ac31e039051dd9e46a03affce3f01b992
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-09-28 15:42:15 -0700

    .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,21 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(5);
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+        root.left.right.left = new TreeNode(7);
+        root.left.right.right = new TreeNode(4);
+        root.right = new TreeNode(1);
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(8);
+
+        levelOrderTraversal levelOrderTraversal = new levelOrderTraversal();
+        levelOrderTraversal.iterativeSolutionLevelOrder(root);
+
+        lowestCommonAncestor lowestCommonAncestor = new lowestCommonAncestor();
+        lowestCommonAncestor.lowestCommonAncestor(root, new TreeNode(5), new TreeNode(4));
 
     }
 
--- src/com/company/TreeAlgorithm/lowestCommonAncestor.java
+++ src/com/company/TreeAlgorithm/lowestCommonAncestor.java
@@ -1,26 +1,27 @@
 package com.company.TreeAlgorithm;
 
 public class lowestCommonAncestor {
-    TreeNode ans;
+    public TreeNode ans;
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        helper(root, p, q);
+        recurseTree(root, p, q);
+        System.out.println(ans.val);
         return ans;
     }
 
-    public boolean helper(TreeNode root, TreeNode p, TreeNode q) {
+    public boolean recurseTree(TreeNode currentNode, TreeNode p, TreeNode q) {
 
-        if (root == null) return false;
+        if (currentNode == null) {
+            return false;
+        }
+        int left = recurseTree(currentNode.left, p, q) ? 1 : 0;
+        int right = recurseTree(currentNode.right, p, q) ? 1 : 0;
+        int mid = (currentNode.val == p.val || currentNode.val == q.val) ? 1 : 0;
+        if (mid + left + right == 2) {
 
-        int left = helper(root.left, p, q) ? 1 : 0;
-        int right = helper(root.right, p, q) ? 1 : 0;
-        if (left + right == 2) {
-            ans = root;
-            return true;
+            ans = currentNode;
         }
-        if (left + right > 0) {
-            return true;
-        }
-        return false;
+
+        return (mid + left + right > 0);
     }
 }
