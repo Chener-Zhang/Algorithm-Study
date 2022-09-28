2571633a89dfda104de8851ab781b2cd2ac508a4
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-09-28 10:40:53 -0700

    + upsideDownBinaryTree.java

---
+++ src/com/company/TreeAlgorithm/upsideDownBinaryTree.java
@@ -0,0 +1,15 @@
+package com.company.TreeAlgorithm;
+
+public class upsideDownBinaryTree {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if (root == null || root.left == null) return root;
+        TreeNode newRoot = upsideDownBinaryTree(root.left);
+
+        root.left.left = root.right;
+        root.left.right = root;
+        root.left = null;
+        root.right = null;
+
+        return newRoot;
+    }
+}
