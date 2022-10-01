0182e29e56b8e81d5054e9285a963bf37ad4892c
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-09-30 22:41:47 -0700

    + 9/30 10:41 recover later

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,18 +7,17 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(3);
-        root.left = new TreeNode(5);
-        root.left.left = new TreeNode(6);
-        root.left.right = new TreeNode(2);
-        root.left.right.left = new TreeNode(7);
-        root.left.right.right = new TreeNode(4);
-        root.right = new TreeNode(1);
-        root.right.left = new TreeNode(0);
-        root.right.right = new TreeNode(8);
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
 
         levelOrderTraversal levelOrderTraversal = new levelOrderTraversal();
         levelOrderTraversal.iterativeSolutionLevelOrder(root);
+
+        treeTraversal treeTraversal = new treeTraversal();
+        treeTraversal.postorder_traversal(root);
 
     }
 
