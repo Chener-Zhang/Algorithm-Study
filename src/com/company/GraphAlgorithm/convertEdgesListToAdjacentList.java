9c1ecdb62d875fe8a0b1bf93c12075ed0d658f55
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-10-01 12:03:09 -0700

    + Compelete convertEdgesListToAdjacentList

--- src/com/company/GraphAlgorithm/convertEdgesListToAdjacentList.java
+++ src/com/company/GraphAlgorithm/convertEdgesListToAdjacentList.java
@@ -8,50 +8,61 @@ public class convertEdgesListToAdjacentList {
     public ArrayList<ArrayList<Integer>> convert_edge_list_to_adjacency_list(Integer n, ArrayList<ArrayList<Integer>> edges) {
         // Write your code here.
         ArrayList<ArrayList<Integer>> res = new ArrayList<>();
-
         for (int i = 0; i < n; i++) {
             res.add(new ArrayList());
         }
-        for (int i = 0; i < n; i++) {
-            res.get(edges.get(i).get(0)).add(edges.get(i).get(1));
-            res.get(edges.get(i).get(1)).add(edges.get(i).get(0));
-
+        for (ArrayList<Integer> pair : edges) {
+            int first = pair.get(0);
+            int second = pair.get(1);
+            res.get(first).add(second);
+            res.get(second).add(first);
         }
-
         for (ArrayList<Integer> pair : res) {
             Collections.sort(pair);
         }
+        System.out.println(res.toString());
         return res;
     }
 
     public void runner() {
         ArrayList<ArrayList<Integer>> edges = new ArrayList<>();
         ArrayList<Integer> arrayList1 = new ArrayList<>();
+        arrayList1.add(2);
         arrayList1.add(0);
-        arrayList1.add(1);
 
         ArrayList<Integer> arrayList2 = new ArrayList<>();
-        arrayList1.add(1);
-        arrayList1.add(4);
+        arrayList2.add(5);
+        arrayList2.add(2);
 
         ArrayList<Integer> arrayList3 = new ArrayList<>();
-        arrayList1.add(1);
-        arrayList1.add(2);
+        arrayList3.add(6);
+        arrayList3.add(2);
 
         ArrayList<Integer> arrayList4 = new ArrayList<>();
-        arrayList1.add(1);
-        arrayList1.add(3);
+        arrayList4.add(2);
+        arrayList4.add(4);
 
         ArrayList<Integer> arrayList5 = new ArrayList<>();
-        arrayList1.add(3);
-        arrayList1.add(4);
+        arrayList5.add(2);
+        arrayList5.add(3);
+
+        ArrayList<Integer> arrayList6 = new ArrayList<>();
+        arrayList6.add(1);
+        arrayList6.add(2);
+
+//        ArrayList<Integer> arrayList7 = new ArrayList<>();
+//        arrayList1.add(3);
+//        arrayList1.add(4);
+
         edges.add(arrayList1);
         edges.add(arrayList2);
         edges.add(arrayList3);
         edges.add(arrayList4);
         edges.add(arrayList5);
+        edges.add(arrayList6);
+//        edges.add(arrayList7);
 
-        convert_edge_list_to_adjacency_list(5, edges);
+        convert_edge_list_to_adjacency_list(7, edges);
 
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import com.company.GraphAlgorithm.convertEdgesListToAdjacentList;
 import com.company.TreeAlgorithm.*;
 
 import java.util.*;
@@ -7,7 +8,8 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-
+        convertEdgesListToAdjacentList convertEdgesListToAdjacentList = new convertEdgesListToAdjacentList();
+        convertEdgesListToAdjacentList.runner();
 
     }
 
