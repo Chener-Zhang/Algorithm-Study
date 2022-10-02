96ead94076ffd6eac318c427cba8c4be9eb031c4
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-10-01 17:02:26 -0700

    + clean up

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,9 @@
 package com.company;
 
-import com.company.GraphAlgorithm.ConvertEdgeListToAdjacencyMatrix;
-import com.company.GraphAlgorithm.graphTestGenerator;
-
-
 public class Main {
 
     public static void main(String[] args) {
 
-        graphTestGenerator graphTestGenerator = new graphTestGenerator();
-
-        ConvertEdgeListToAdjacencyMatrix ConvertEdgeListToAdjacencyMatrix = new ConvertEdgeListToAdjacencyMatrix();
-        ConvertEdgeListToAdjacencyMatrix.convert_edge_list_to_adjacency_matrix(5,graphTestGenerator.toArrayList(new int[][]{{0, 1}, {1, 4}, {1, 2}, {1, 3}, {3, 4}}));
     }
 
 
