cb9c81013f92189caf778073ba80b1c4c161ac29
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-10-04 21:10:32 -0700

    + FloodFill

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,14 +2,35 @@ package com.company;
 
 import com.company.GraphAlgorithm.FloodFill;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
         FloodFill FloodFill = new FloodFill();
-        FloodFill.floodFill(new int[][]{{0,2,1}, {1, 1, 2}, {2, 5,4}}, 1, 0, 9);
+        FloodFill.floodFill(new int[][]{{0, 2, 1}, {1, 1, 2}, {2, 5, 4}}, 1, 0, 9);
 
     }
 
+    static ArrayList<ArrayList<Integer>> flood_fill(Integer pixel_row, Integer pixel_column, Integer new_color, ArrayList<ArrayList<Integer>> image) {
+        // Write your code here.
+        if (new_color != image.get(pixel_row).get(pixel_column))
+            dfs(image, pixel_row, pixel_column, new_color, image.get(pixel_row).get(pixel_column));
+        return image;
+    }
+
+    static void dfs(ArrayList<ArrayList<Integer>> image, Integer sr, Integer sc, Integer color, Integer orginalColor) {
+        if (sr < 0 || sc < 0 || sr == image.size() || sc == image.get(0).size()) return;
+        if (image.get(sr).get(sc) == orginalColor) {
+            image.get(sr).set(sc, color);
+            dfs(image, sr - 1, sc, color, orginalColor);
+            dfs(image, sr + 1, sc, color, orginalColor);
+            dfs(image, sr, sc + 1, color, orginalColor);
+            dfs(image, sr, sc - 1, color, orginalColor);
+        } else {
+            return;
+        }
+    }
 
 }
