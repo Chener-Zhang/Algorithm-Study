6618a92f93e2fc453430d90617657d61cf747e51
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-10-07 12:40:05 -0700

    -- Clean up

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,56 +1,9 @@
 package com.company;
 
-import com.company.GraphAlgorithm.minKnightMoves;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        minKnightMoves minKnightMoves = new minKnightMoves();
-        System.out.println(minKnightMoves.minKnightMoves(5, 5));
-
-
-    }
-
-
-    static ArrayList<Integer> res;
-
-    static ArrayList<Integer> course_schedule(Integer n, ArrayList<ArrayList<Integer>> prerequisites) {
-        // Write your code here.
-        LinkedList<Integer>[] edgesList = new LinkedList[n];
-        for (int i = 0; i < n; i++) {
-            edgesList[i] = new LinkedList();
-        }
-
-        for (ArrayList<Integer> list : prerequisites) {
-            edgesList[list.get(1)].add(list.get(0));
-        }
-
-        int[] visited = new int[n];
-        res = new ArrayList<Integer>();
-
-        for (int i = 0; i < n; i++) {
-            if (!dfs(edgesList, visited, i)) return new ArrayList();
-        }
-
-
-        return res;
-
-    }
-
-    static boolean dfs(LinkedList<Integer>[] edgesList, int[] visited, int start) {
-        if (visited[start] == 2) return true;
-        if (visited[start] == 1) return false;
-        visited[start] = 1;
-        for (int neighbor : edgesList[start]) {
-            if (!dfs(edgesList, visited, neighbor)) return false;
-        }
-        visited[start] = 2;
-        res.add(start);
-        return true;
     }
 
 
--- src/com/company/debug.java
+++ src/com/company/debug.java
@@ -1,33 +1,6 @@
 package com.company;
 
-import com.company.TreeAlgorithm.TreeNode;
-
-import java.util.*;
-
 public class debug {
-    public List<String> binaryTreePaths(TreeNode root) {
-        List<String> list = new ArrayList<String>();
-        Queue<TreeNode> qNode = new LinkedList<TreeNode>();
-        Queue<String> qStr = new LinkedList<String>();
 
-        if (root == null) return list;
-        qNode.add(root);
-        qStr.add("");
-        while (!qNode.isEmpty()) {
-            TreeNode curNode = qNode.remove();
-            String curStr = qStr.remove();
-
-            if (curNode.left == null && curNode.right == null) list.add(curStr + curNode.val);
-            if (curNode.left != null) {
-                qNode.add(curNode.left);
-                qStr.add(curStr + curNode.val + "->");
-            }
-            if (curNode.right != null) {
-                qNode.add(curNode.right);
-                qStr.add(curStr + curNode.val + "->");
-            }
-        }
-        return list;
-    }
 
 }
