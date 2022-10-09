4ab4980109972640a528cae1000c827c73a918d8
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-10-09 10:39:00 -0700

    + static maximum_path_sum [Completed]

--- src/com/company/DynamicProgramming/minPathSum.java
+++ src/com/company/DynamicProgramming/minPathSum.java
@@ -1,5 +1,7 @@
 package com.company.DynamicProgramming;
 
+import java.util.ArrayList;
+
 public class minPathSum {
     public int minPathSum(int[][] grid) {
         int[][] arr = new int[grid.length][grid[0].length];
@@ -16,7 +18,25 @@ public class minPathSum {
                 arr[i][j] = Math.min(grid[i][j] + arr[i - 1][j], grid[i][j] + arr[i][j - 1]);
             }
         }
-//        System.out.println(arr[grid.length - 1][grid[0].length - 1]);
         return arr[grid.length - 1][grid[0].length - 1];
+    }
+
+    public Integer maximum_path_sum(ArrayList<ArrayList<Integer>> grid) {
+        // Write your code here.
+        int rowLength = grid.get(0).size();
+        int colLength = grid.size();
+        //1 2 3
+        for (int i = 1; i < rowLength; i++) {
+            grid.get(0).set(i, grid.get(0).get(i - 1) + grid.get(0).get(i));
+        }
+        for (int i = 1; i < colLength; i++) {
+            grid.get(i).set(0, grid.get(i - 1).get(0) + grid.get(i).get(0));
+        }
+        for (int i = 1; i < colLength; i++) {
+            for (int j = 1; j < rowLength; j++) {
+                grid.get(i).set(j, Math.max(grid.get(i).get(j) + grid.get(i - 1).get(j), grid.get(i).get(j) + grid.get(i).get(j - 1)));
+            }
+        }
+        return grid.get(colLength - 1).get(rowLength - 1);
     }
 }
