b1ee9a8e79470ab58bdc791a133c06073f476a5e
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-10-10 18:10:35 -0700

    + coinChangeII [Completed]

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,20 @@
 package com.company;
 
 
-import com.company.DynamicProgramming.rodCutting;
+import com.company.DynamicProgramming.coinChangeII;
 
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        rodCutting rodCutting = new rodCutting();
+        coinChangeII rodCutting = new coinChangeII();
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(5);
         list.add(8);
         list.add(9);
+        System.out.println(rodCutting.change(5, new int[]{1, 2, 5}));
 
-
-        System.out.println(rodCutting.get_maximum_profit(list));
     }
 }
