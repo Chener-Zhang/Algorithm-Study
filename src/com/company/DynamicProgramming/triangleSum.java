41ccf40f2f0a0ab0720cce007257f36f096ff5ad
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-10-11 13:33:58 -0700

    + triangleSum [Completed]

--- src/com/company/DynamicProgramming/triangleSum.java
+++ src/com/company/DynamicProgramming/triangleSum.java
@@ -1,25 +1,17 @@
 package com.company.DynamicProgramming;
 
-import java.util.Collections;
-import java.util.List;
-
 public class triangleSum {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int size = triangle.size();
-        for (int row = 1; row < size; row++) {
-            for (int col = 0; col <= row; col++) {
-
-                if (col == 0) {
-                    triangle.get(row).set(0, triangle.get(row - 1).get(0) + triangle.get(row).get(0));
-                } else if (col == row) {
-                    triangle.get(row).set(col, triangle.get(row - 1).get(col - 1) + triangle.get(row).get(col));
-                } else {
-                    triangle.get(row).set(col, Math.min(triangle.get(row).get(col) + triangle.get(row - 1).get(col - 1), triangle.get(row).get(col) + triangle.get(row - 1).get(col)));
-                }
-            }
+    public int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
         }
-        System.out.println(triangle.toString());
-        Collections.sort(triangle.get(size - 1));
-        return triangle.get(size - 1).get(0);
+        int[] arr = new int[nums.length];
+        arr[0] = nums[0];
+        arr[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            arr[i] = Math.max(arr[i - 1], arr[i - 2] + nums[i]);
+        }
+        return arr[nums.length - 1];
     }
 }
