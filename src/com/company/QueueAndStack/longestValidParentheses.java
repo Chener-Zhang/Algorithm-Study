3410d1afb42eb036944c98ca101ecb09b7d96547
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-10-18 09:26:55 -0700

    + longestValidParentheses [Completed]

---
+++ src/com/company/QueueAndStack/longestValidParentheses.java
@@ -0,0 +1,22 @@
+package com.company.QueueAndStack;
+
+import java.util.Stack;
+
+public class longestValidParentheses {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack();
+        stack.push(-1);
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) stack.push(i);
+                else max = Math.max(max, i - stack.peek());
+            }
+        }
+        return max;
+    }
+}
