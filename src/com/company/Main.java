671e7b82305b84e8f29dafec99fd3e60806f14c2
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-10-25 09:41:29 -0700

    + longestConsecutive [Completed]

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,12 @@
 package com.company;
 
 
-import com.company.TreeAlgorithm.TreeNode;
-import com.company.TreeAlgorithm.flattenBinaryTree;
-import com.company.TreeAlgorithm.levelOrderTraversal;
+import com.company.Array.longestConsecutive;
+import com.company.DynamicProgramming.canJump;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        flattenBinaryTree flattenBinaryTree = new flattenBinaryTree();
-        TreeNode treeNode = new TreeNode(1);
-        treeNode.left = new TreeNode(2);
-        treeNode.right = new TreeNode(5);
-        treeNode.left.left = new TreeNode(3);
-        treeNode.left.right = new TreeNode(4);
-        treeNode.right.right = new TreeNode(6);
-        levelOrderTraversal levelOrderTraversal = new levelOrderTraversal();
-        levelOrderTraversal.iterativeSolutionLevelOrder(treeNode);
-        flattenBinaryTree.flatten(treeNode);
 
     }
 }
