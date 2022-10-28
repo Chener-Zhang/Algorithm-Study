83a00c30bf3b4857c71a7adab5ff7c2c160fb7d7
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-10-28 11:13:56 -0700

    + moveZeroes [Completed]

---
+++ src/com/company/Array/moveZeroes.java
@@ -0,0 +1,18 @@
+package com.company.Array;
+
+public class moveZeroes {
+    public void moveZeroes(int[] nums) {
+        int lastZero = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[lastZero];
+                nums[lastZero] = temp;
+                lastZero++;
+            }
+        }
+
+    }
+
+
+}
