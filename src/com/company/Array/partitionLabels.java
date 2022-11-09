ef1bcf85c76f10bf7484313f313487fc866ec8d9
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-11-09 11:32:00 -0800

    + partitionLabels [Complete] Using Array hashmap

--- src/com/company/Array/partitionLabels.java
+++ src/com/company/Array/partitionLabels.java
@@ -3,23 +3,22 @@ package com.company.Array;
 import com.company.LeetcodeProblem;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class partitionLabels implements LeetcodeProblem {
     public List<Integer> partitionLabels(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
+        int[] map = new int[26];
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            map.put(c, map.getOrDefault(0, i));
+            map[c - 'a'] = i;
         }
         int j = 0;
         int previous = 0;
         List<Integer> res = new ArrayList<>();
         for (int i = 0; i < s.length(); i++) {
-            j = Math.max(j, map.get(s.charAt(i)));
+            j = Math.max(j, map[s.charAt(i) - 'a']);
             if (j == i) {
-                res.add(j - previous);
+                res.add(j - previous + 1);
                 previous = i + 1;
             }
         }
