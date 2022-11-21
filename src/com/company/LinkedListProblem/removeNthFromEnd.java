3e91fbd3a05e0b28a075a520aab2a80dd660457f
CHENER ZHANG <55104326+Chener-Zhang@users.noreply.github.com>
2022-11-21 09:50:47 -0800

    + removeNthFromEnd motification

--- src/com/company/LinkedListProblem/removeNthFromEnd.java
+++ src/com/company/LinkedListProblem/removeNthFromEnd.java
@@ -1,8 +1,30 @@
 package com.company.LinkedListProblem;
 
+import com.company.LeetcodeProblem;
+import com.company.Tools;
 import com.company.TreeAlgorithm.ListNode;
 
-public class removeNthFromEnd {
+public class removeNthFromEnd implements LeetcodeProblem {
+    Tools tools = new Tools();
+
+    public ListNode removeNthFromEndII(ListNode head, int n) {
+        ListNode pre = new ListNode(-1);
+        pre.next = head;
+        ListNode first = pre.next;
+        ListNode second = pre;
+        for (int i = 1; i < n + 1; i++) {
+            first = first.next;
+        }
+
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return pre.next;
+
+    }
+
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode toRemove = null;
         ListNode current = head;
@@ -22,5 +44,10 @@ public class removeNthFromEnd {
         if (pre == null) return head.next;
         pre.next = toRemove.next;
         return head;
+    }
+
+    @Override
+    public void run() {
+        removeNthFromEndII(tools.listNodeCreator(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}), 3);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,12 @@
 package com.company;
 
 
-import com.company.DynamicProgramming.canPartition;
+import com.company.LinkedListProblem.removeNthFromEnd;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        LeetcodeProblem leetcodeProblem = new removeNthFromEnd();
+        leetcodeProblem.run();
     }
 }
